48d8e57f9329d02a8157b2dcddc13cb8d798ace2
ArfinHasib <arfinhasib21375@gmail.com>
2025-06-01 21:49:01 +0600

    defining custom color properties

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -27,7 +27,23 @@ export const loginItems = [
 ];
 
 export const Header = () => {
-  return <header>header section</header>;
+  return <header className="border-b border-gray-200/20">
+   <div className="container">
+      <div className="h-18 flex items-center">
+         <div className="flex gap-4 items-center">
+            <div className="size-10 bg-gray-200 bg-[conic-gradient[from_45deg,red,green,blue]]"></div>
+            <div className="font-extrabold text-2xl">sphereal.ai</div>
+         </div>
+         <div></div>
+         <div>
+            <button>
+               <div></div>
+               <div></div>
+            </button>
+         </div>
+      </div>
+   </div>
+  </header>;
 };
 
 export default Header;
